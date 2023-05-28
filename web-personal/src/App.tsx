9f75cff086dc678e59e076246ac1933f23bc8884
jukka <jukka.a.r@hotmail.com>
2023-05-28 15:00:06 +0200

    a

--- web-personal/src/App.tsx
+++ web-personal/src/App.tsx
@@ -31,7 +31,7 @@ function App() {
             <h1>Bienvenido a mi Web</h1>
             <p>Mi nombre es Jukka Ahola, he estudiado DAM y busco ser programador</p>
             <div className="image-container">
-            <img src="src/assets/hombre_programando.gif" alt="Description of the image" />
+            <img src="/hombre_programando.gif" alt="Description of the image" />
           </div>
           </section>
 
