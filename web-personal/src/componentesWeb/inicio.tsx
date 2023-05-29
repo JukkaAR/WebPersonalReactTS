dd4e2c075842cf28a1bc5904dba78b777c2a5bd3
jukka <jukka.a.r@hotmail.com>
2023-05-29 18:23:18 +0200

    a

--- web-personal/src/componentesWeb/inicio.tsx
+++ web-personal/src/componentesWeb/inicio.tsx
@@ -2,14 +2,10 @@ export const Inicio = () => {
   return (
     <section id="home">
       <h1>Bienvenido a mi Web</h1>
-      <div className="content-container">
-        <h2>
-          Mi nombre es Jukka Ahola, he estudiado DAM y busco ser programador
-        </h2>
-        <div className="image-container">
-          <img src="/hombre_programando.gif" alt="Description of the image" />
-        </div>
-      </div>
+      <h2>
+        Mi nombre es Jukka Ahola, he estudiado DAM y busco ser programador
+      </h2>
+      <img src="/hombre_programando.gif" alt="Description of the image" />
     </section>
   );
 };
