4d22429d7700e0d97764b58fef88b93f8fb13be1
jukka <jukka.a.r@hotmail.com>
2023-05-31 20:33:26 +0200

    Iconos

--- web-personal/src/componentesWeb/navegacion.tsx
+++ web-personal/src/componentesWeb/navegacion.tsx
@@ -1,21 +1,27 @@
+import { SiGooglehome } from "react-icons/si";
+import { SiAboutdotme } from "react-icons/si";
+import { SiBookstack } from "react-icons/si";
+import { SiGithub } from "react-icons/si";
+import { SiMessenger } from "react-icons/si";
+
 export const Navegacion = () => {
   return (
     <nav>
       <ul className="horizontal-list fixed-top">
         <li>
-          <a href="#home">Inicio</a>
+          <a href="#home"><SiGooglehome/></a>
         </li>
         <li>
-          <a href="#knowledge">Conocimientos</a>
+          <a href="#knowledge"><SiBookstack/></a>
         </li>
         <li>
-          <a href="#about">Sobre mí</a>
+          <a href="#about"><SiAboutdotme/></a>
         </li>
         <li>
-          <a href="#projects">Proyectos</a>
+          <a href="#projects"><SiGithub/></a>
         </li>
         <li>
-          <a href="#contact">Contactar</a>
+          <a href="#contact"><SiMessenger/></a>
         </li>
       </ul>
     </nav>
