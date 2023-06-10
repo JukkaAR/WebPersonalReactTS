d66f5aecbfa7a3c703bc57cb0b160583edf52e4f
jukka <jukka.a.r@hotmail.com>
2023-06-11 00:50:27 +0200

    a

--- web-personal/src/componentesWeb/inicio.tsx
+++ web-personal/src/componentesWeb/inicio.tsx
@@ -1,16 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 export const Inicio: React.FC = () => {
   //Animación de escritura
-  const [text, setText] = useState<string>('');
+  const [text, setText] = useState<string>("");
   const [index, setIndex] = useState<number>(0);
-  const texts: string[] = ["Jukka Ahola","programador", "chileno", "joven","finlandés"];
+  const texts: string[] = [
+    "Jukka Ahola",
+    "programador",
+    "chileno",
+    "joven",
+    "finlandés",
+  ];
   useEffect(() => {
     const typingInterval = setInterval(() => {
       const currentText = texts[index];
       typeText(currentText, 0);
       setIndex((prevIndex) => (prevIndex + 1) % texts.length);
-    },1000 + texts.length * 400); // Adjust the delay based on text length
+    }, 1000 + texts.length * 400); // Adjust the delay based on text length
 
     return () => {
       clearInterval(typingInterval);
@@ -33,14 +39,19 @@ export const Inicio: React.FC = () => {
   return (
     <section id="home">
       <h1>Bienvenido a mi Web</h1>
+      <div className="flex-container">
       <div>
-      <h2 className='align-left'>
-        Soy {text}<span className="cursor"/> {/* Vertical rectangle cursor */}
-      </h2>
+        <h2 className="align-left" style={{ width: "400px" }}>
+          Soy {text}
+          <span className="cursor" /> {/* Vertical rectangle cursor */}
+        </h2>
       </div>
-      <img src="/hombre_programando.gif" alt="Gif Hombre programando" className='responsive-image'/>
-      
+      <img
+        src="/hombre_programando.gif"
+        alt="Gif Hombre programando"
+        className="responsive-image"
+      />
+      </div>
     </section>
-    
   );
 };
--- web-personal/src/componentesWeb/sobreMi.tsx
+++ web-personal/src/componentesWeb/sobreMi.tsx
@@ -10,6 +10,7 @@ export const SobreMi = () => {
         <div className="square-container" data-aos="fade-left">
             <section id="about">
             <h2>Sobre mí</h2>
+            
             <p>Tengo 23 años, soy finlandés y chileno aunque resido actualmente en Valencia<br/>
             Me encanta trabajar tanto en equipo como individualmente.<br/> 
             Valoro mucho la sinceridad y el respeto, ya que no hay solución a un problema que no queremos reconocer y cuando cooperamos somos mucho más eficientes</p>
