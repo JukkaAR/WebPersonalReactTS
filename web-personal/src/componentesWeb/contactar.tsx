0353c1bd83c0959ed322eeda5afdcd19c2408523
jukka <jukka.a.r@hotmail.com>
2023-06-13 12:21:05 +0200

    a

--- web-personal/src/componentesWeb/contactar.tsx
+++ web-personal/src/componentesWeb/contactar.tsx
@@ -12,15 +12,19 @@ export const Contactar = () => {
             <section id="contact">
             <h2>Contactar</h2>
             <form name="contact" method="POST" data-netlify="true">
-              <input type="hidden" name="form-name" value="contact" />
-              <input type="text" id="name" name="name" required />
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" required />
-              <label htmlFor="message">Mensaje:</label>
-              <textarea id="message" name="message" required></textarea>
-              <button type="submit">Enviar mensaje</button>
-  {/* Rest of the form fields */}
-            </form>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
           </section>
         </div>
     );
