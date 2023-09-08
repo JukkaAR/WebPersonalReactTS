48d8288a5d75977484a73ccf1c58b996a62e8274
JukkaAR <jukka.a.r@hotmail.com>
2023-09-09 00:46:12 +0200

    Helmet install

--- web-personal/src/componentesWeb/tutoriales.tsx
+++ web-personal/src/componentesWeb/tutoriales.tsx
@@ -2,6 +2,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import { Tutoriali18n } from "./tutoriales/tutoriali18n";
+import { Helmet } from 'react-helmet';
+
 
 export const Tutoriales = () => {
   useEffect(() => {
@@ -11,6 +13,9 @@ export const Tutoriales = () => {
     <>
       <div className="spacer" />
       <div>
+        <Helmet>
+          <link rel="preconnect" href="https://jukka-ahola.netlify.app/" />
+        </Helmet>
         <Tutoriali18n />
       </div>
     </>
